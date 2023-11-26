189dbb6bcbc6f4d7eeb4613c91a7523cca159412
ekanth23 <ekanthn@gmail.com>
2023-11-26 22:05:00 +0530

    First Time Push

---
+++ src/test/java/org/testclass/ClassA.java
@@ -0,0 +1,15 @@
+package org.testclass;
+
+public class ClassA {
+	
+	private void method1() {
+		System.out.println("Method-1");
+	}
+	private void method2() {
+		System.out.println("Method-2");
+	}
+	private void method3() {
+		System.out.println("Method-3");
+	}
+
+}
